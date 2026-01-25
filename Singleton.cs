d7ad91de48dd0995cb49c8f1144c981ef6062e8e
AkashPandey8789 <akashpandeyssm71@gmail.com>
2026-01-25 19:01:49 +0530

    Lazy<T> implementation of Singleton pattern

--- Singleton.cs
+++ Singleton.cs
@@ -1,8 +1,10 @@
 public sealed class Singleton
 {
-    private static Singleton _singleton;
+    // private static Singleton _singleton;
 
-    private static readonly object InstanceLock = new object();
+    // private static readonly object InstanceLock = new object();
+
+    private static Lazy<Singleton> singleton = new Lazy<Singleton>(() => new Singleton());
     private static int counter = 0;
 
     private Singleton()
@@ -13,20 +15,24 @@ public sealed class Singleton
 
     public static Singleton GetInstance()
     {
-        if (_singleton == null) //this check will allow multi-threading once the instance is created by any one thread...
-        {
-            lock (InstanceLock) //this slows the system as only one thread can access the instance at once....
-            {
-                if (_singleton == null)  // if the instance is not created we are checking just to be safe in meantime no other thread created instance
-                {
-                    _singleton = new Singleton();
-                    //return new Singleton();
-                }
-            }
-        }
-        return _singleton;
+        return singleton.Value;
     }
-    
+    // public static Singleton GetInstance()
+    // {
+    //     if (_singleton == null) //this check will allow multi-threading once the instance is created by any one thread...
+    //     {
+    //         lock (InstanceLock) //this slows the system as only one thread can access the instance at once....
+    //         {
+    //             if (_singleton == null)  // if the instance is not created we are checking just to be safe in meantime no other thread created instance
+    //             {
+    //                 _singleton = new Singleton();
+    //                 //return new Singleton();
+    //             }
+    //         }
+    //     }
+    //     return _singleton;
+    // }
+
     public void Print(string message)
     {
         Console.WriteLine($"{message}");
